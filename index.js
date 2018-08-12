88492db46f6d8587e829cc237fbb60b1ebb2670b
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-12 01:13:48 -0700

    (feat( Enable json posts

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+express.json();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -6,8 +9,6 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res, next) => {
   return res.send('<h1>Hello User</h1>');
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
